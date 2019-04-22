49f300854bc1a66368b891585ed90067a7212b82
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-22 16:55:11 -0700

    works index - working on table

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,9 @@
 class HomepageController < ApplicationController
   def index
-    @books = Work.where(category: "book")
-    @movies = Work.where(category: "movie")
-    @albums = Work.where(category: "album")
+    @books = Work.where(category: "book").sample(10)
+    @movies = Work.where(category: "movie").sample(10)
+    @albums = Work.where(category: "album").sample(10)
+
+    @featured = Work.all.sample
   end
 end
--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe HomepageController do
   describe "index" do
     it "shows the index page" do
-      get works_path
+      get root_path
       must_respond_with :ok
     end
   end
