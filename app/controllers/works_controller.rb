94aaf17b332d62be847577143d023710cc4be32b
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-23 15:00:54 -0700

    create page and flash error display

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,30 @@ class WorksController < ApplicationController
     @movies = Work.where(category: "movie")
     @albums = Work.where(category: "album")
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    success = @work.save
+
+    if success
+      flash[:status] = :success
+      flash[:message] = "Successfully created #{@work.category} #{@work.id}"
+      redirect_to works_path
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not create #{@work.category}"
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :creator, :category, :publication_year, :description)
+  end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -7,4 +7,31 @@ describe WorksController do
       must_respond_with :ok
     end
   end
+
+  describe "new" do
+    it "must load the page" do
+      get new_work_path
+      must_respond_with :ok
+    end
+  end
+
+  describe "create" do
+    let(:work_data) {
+      {
+        work: {
+          title: "It",
+          category: "book",
+          creator: "Stephen King",
+        },
+      }
+    }
+    it "must create the work" do
+      expect {
+        post works_path(params: work_data)
+      }.must_change "Work.count", 1
+
+      work = Work.last
+      expect(work.title).must_equal "It"
+    end
+  end
 end
