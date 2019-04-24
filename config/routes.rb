b5e59fd9960acb1e9d7ff066347d5d4db92a17be
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-24 15:32:10 -0700

    created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root "works#top_works"
 
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe "validation" do
+    before do
+      @user = User.first
+    end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it "must be valid" do
+      value(@user).must_be :valid?
+    end
+
+    it "needs a username" do
+      @user.username = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :username
+    end
   end
 end
