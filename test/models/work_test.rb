ad9a67eae83fa7f97ff8b7c40e50ce9e0d153bd9
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-04-28 12:33:20 -0700

    added tests for custom methods in work.rb

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -31,6 +31,35 @@ describe Work do
 
   describe "custom methods" do
     it "finds the work with the most votes" do
+      featured = Work.top_work
+      # the top work in the fixtures file has 10 votes
+      expect(featured.votes_count).must_equal 10
+    end
+
+    it "top_ten gets a list of ten works" do
+      12.times do |i|
+        Work.create!(title: i, category: "movie", creator: "no one", votes_count: i)
+      end
+
+      works_array = Work.top_ten("movie")
+
+      expect(works_array.count).must_equal 10
+      expect(works_array.first.votes_count).must_equal 11
+    end
+
+    it "top_ten gets all the records if there are fewer than 10" do
+      works_array = Work.top_ten("movie")
+
+      # there is 1 movie in the fixtures db
+      expect(works_array.count).must_equal 1
+    end
+
+    it "top_ten returns an empty array if there are no works in the category" do
+      works_array = Work.top_ten("album")
+
+      # there are no albums in the fixtures db
+
+      expect(works_array.count).must_equal 0
     end
   end
 end
